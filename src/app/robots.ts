12f67edd216ef99258ff003a4784e3b1177d533b
Upendra Rai <upendrarai02@gmail.com>
2026-08-26 05:22:04 +0530

    just fixed robots.ts file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,12 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://upendrarai.dev'
+  const base =
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    'https://upendra-rai.github.io/java-portfolio'
+
   return {
     rules: [{ userAgent: '*', allow: '/' }],
     sitemap: `${base}/sitemap.xml`,
